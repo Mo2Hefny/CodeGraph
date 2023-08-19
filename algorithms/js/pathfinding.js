77ae07d1e961f3c3885a96fe07c41f1fae47e5a3
Mo2Hefny <moamensaied1@gmail.com>
2023-08-19 15:04:11 +0300

    Add grid generation

--- algorithms/js/pathfinding.js
+++ algorithms/js/pathfinding.js
@@ -1,33 +1,49 @@
 let blocks = [];
-let CurrSize = 2;
+let selectedMode = "";
+let startedSimulation = false;
 const grid = document.querySelector('.animation-area');
+const sizeSlider = document.getElementById('size-range');
+setGrid(sizeSlider.value);
+const speedSlider = document.getElementById('speed-range');
+const setStartButton = document.getElementById('set-start');
+const setEndButton = document.getElementById('set-end');
+const setWallButton = document.getElementById('set-wall');
 
-setGrid(CurrSize);
+setStartButton.addEventListener('click', setSelectedMode);
+function toggle(b){b.value=(b.value=="ON")?"OFF":"ON";}
+
+function setSelectedMode(event) {
+  const ID = event.type.getAttribute('id');
+  
+}
+
 function setGrid(size)
 {
-  const sizeEm = size * 16;
-  let columnSize = grid.offsetWidth / sizeEm;
-  let rowSize = grid.offsetHeight / sizeEm;
-  for (let i = 0; i < rowSize; i++)
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  for (let i = 0; i < size; i++)
   {
-    let row = document.createElement('div');
-    row.classList.add('block-row');
-    row.style.width = '100%';
     blocks[i] = [];
-    for (let j = 0; j < columnSize; j++)
+    for (let j = 0; j < size; j++)
     {
-      const block = createGridBlock(sizeEm - 4);
+      const block = createGridBlock(i, j, size);
       blocks[i][j] = block;
-      row.appendChild(block);
+      grid.appendChild(block);
     }
-    grid.appendChild(row);
   }
 }
 
-function createGridBlock(size) {
-  const block = document.createElement('div');
-  const GAP = 4;
-  block.classList.add('block');
+function createGridBlock(i, j, size) {
+  const block = document.createElement('cell');
+  if (i === 0) block.classList.add('top');
+  if (i === size - 1) block.classList.add('bottom');
+  if (j === 0) block.classList.add('left');
+  if (j === size - 1) block.classList.add('right');
   grid.appendChild(block);
   return block;
+}
+
+function editGrid(event) {
+  const cell = event.type.closest('cell');
+  if (cell === undefined || startedSimulation) return;
+  
 }
