4d88273b1087cd94b448d92294303359a56024ac
Mo2Hefny <moamensaied1@gmail.com>
2023-08-22 18:27:48 +0300

    Add script for toggling menu

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const icon = document.querySelector('.hamburger-icon');
+  const items = document.querySelector('.hamburger-items');
+  icon.classList.toggle('open');
+  items.classList.toggle('open');
+}
